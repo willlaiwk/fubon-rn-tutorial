d7a235d8228c74979b92c63e7dbd5039b822f3f8
Will Lai <will.lai.wk@gmail.com>
2018-04-18 11:54:43 +0800

    update server

--- rn-customer-app-server/index.js
+++ rn-customer-app-server/index.js
@@ -2,13 +2,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const customers = require('./customers');
 
+const fakeUser = { username: 'admin', password: '123456' };
+
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/api/login', (req, res) => {
+  const { account, password } = req.body;
+  if (account === fakeUser.username && password === fakeUser.password) {
+    res.send({ token: 'testtokendsfdsdsvlsdvsdjlsdvjslvjfsjdlfj', salt: 'sddd' });
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+
 app.get('/api/customers', (req, res) => {
   res.send(customers);
 });
+
+app.post('/api/customers', (req, res) => {
+  const id = customers.reduce((prevId, current) => Math.max(prevId, current.id), 0);
+  const customer = { ...req.body, id };
+  customers.push(customer);
+  res.send({ result: true, customer });
+});
+
+app.put('/api/customers/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const customerIndex = customers.findIndex(m => m.id == id);
+
+  if (customerIndex >= 0) {
+    const customer = { ...customers[customerIndex], ...req.body };
+    customers[customerIndex] = customer;
+    res.send({ result: true, customer });
+  } else {
+    res.status(400).send('找不到 customer !');
+  }
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const customerIndex = customers.findIndex(m => m.id == id);
+  if (customerIndex >= 0) {
+    customers.splice(customerIndex, 1);
+    res.send({ result: true, customers });
+  } else {
+    res.status(400).send('找不到 customer !');
+  }
+});
+
 app.listen(7654, () => {
   console.log('Server is running on port 7654.');
 });
